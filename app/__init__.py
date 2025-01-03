8ad90d0f3818fdb19484d704928aa4fba6880be1
jimi-117 <sho.jimi117@gmail.com>
2025-01-03 17:36:22 +0100

    clean up architecture

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,9 @@ from flask import Flask
 def create_app(test_config=None):
     # app configuration
     app = Flask(__name__, instance_relative_config=False)
-
-    # load the instance config, if it exists, when not testing
     
     app.config.from_pyfile('config.py')
 
-    # ensure the instance folder exists
     dictConfig(app.config["LOGGING"])
     
     return app
